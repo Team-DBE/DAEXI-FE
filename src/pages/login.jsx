9b5d8d680488ea0ed837d560c54df769d40dd4b1
yunho09 <jyh19310302@gmail.com>
2025-07-16 18:16:51 +0900

    Login.jsx 추가
    Login.jsx 추가

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,22 +1,18 @@
 import styled from "styled-components";
 
 function Login() {
-    return 
+  return (
+    <>
+      <Logindiv />
+    </>
+  );
 }
 
-const Login
+const Logindiv = styled.div`
+  width: 788px;
+  height: 777px;
+  padding: 0;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-export default Login();
-
+export default Login;

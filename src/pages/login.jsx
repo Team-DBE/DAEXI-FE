9eb5791a543be6271a66400bfc3b722b05abe16f
yunho09 <jyh19310302@gmail.com>
2025-07-31 21:42:19 +0900

    reset.css 추가 login 완료
    reset.css를 추가했고 login페이지 퍼블리싱 완성

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,51 +1,133 @@
 import styled from "styled-components";
+import "../components/reset.css";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+`;
 
 function Login() {
   return (
     <Body>
+      <GlobalStyle />
       <Logo src="/src/assets/Logo/Logo_small.svg"></Logo>
-      <Logindiv>
+      <LoginDiv>
         <Logintitle>로그인</Logintitle>
-        <Inputbox />
-      </Logindiv>
+        <Wrapper>
+          <Inputbox type="text" placeholder="아이디" />
+          <Forget>아이디를 잊어버렸어요</Forget>
+          <Inputbox type="password" placeholder="비밀번호" />
+          <Forget>비밀번호를 잊어버렸어요</Forget>
+        </Wrapper>
+        <ButtonWrapper>
+          <Button bgColor="#FF8C00">로그인</Button>
+          <Line>또는</Line>
+          <Button bgColor="rgba(0,0,0,0.3)">회원가입</Button>
+        </ButtonWrapper>
+      </LoginDiv>
     </Body>
   );
 }
 
 const Body = styled.div`
   display: flex;
+  padding: 0;
+  margin: 0;
   align-items: center;
+  justify-content: center;
   flex-direction: column;
+  height: 100vh;
 `;
 
-const Logindiv = styled.div`
+const LoginDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 750px;
+  width: 600px;
   height: 700px;
-  padding: 0;
   border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
 `;
 
 const Logintitle = styled.h1`
+  margin-top: 70px;
+  font-weight: 600;
   font-size: 50px;
 `;
 
 const Logo = styled.img`
   height: 80px;
-  margin-top: 50px;
-  margin-bottom: 20px;
+  margin-left: 30px;
+  margin-bottom: 50px;
 `;
 
-const Inputbox = styled.div`
-  width: 550px;
-  height: 80px;
+const Wrapper = styled.div`
+  width: 400px;
+  margin-top: 20px;
+`;
+
+const Inputbox = styled.input`
+  width: 100%;
+  height: 60px;
+  margin-top: 30px;
+  padding-top: 5px;
+  padding-left: 30px;
+  padding-right: 30px;
+  font-size: 25px;
   border: 1px solid rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
 `;
 
-const Submitbox = styled.div`
-  
+const Forget = styled.h2`
+  padding-top: 15px;
+  font-size: 16px;
+  color: rgba(0, 0, 0, 0.5);
+  text-align: right;
+  cursor: pointer;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 400px;
+  margin-top: 50px;
+`;
+
+const Button = styled.button`
+  border: none;
+  border-radius: 10px;
+  width: 100%;
+  height: 60px;
+  background-color: ${(props) => props.bgColor};
+  color: white;
+  font-size: 25px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+const Line = styled.div`
+  display: flex;
+  align-items: center;
+  color: rgba(0,0,0,0.3);
+  font-size: 17px;
+  font-weight: 500;
+  margin: 20px 0;
+
+  &::before,
+  &::after {
+    content: "";
+    flex: 1;
+    height: 1px;
+    background: rgba(0,0,0,0.3);
+  }
+
+  &::before {
+    margin-right: 12px;
+  }
+
+  &::after {
+    margin-left: 12px;
+  }
 `;
 
 export default Login;
